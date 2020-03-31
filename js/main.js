f7c1714c34c439d7818f4c66ff0fe07d741d1db2
Nicola <niconix.dev@gmail.com>
2020-03-31 19:05:43 +0200

    Css

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var newSurname = document.getElementById('new-surname');
 var surnameList = document.getElementById('surname-list');
 var btnAdd = document.getElementById('btnAdd');
 var list = ['Bianchi', 'Rossi', 'Dunzioni', 'Balsano', 'Verdi'];
+
 //  inserimento nuovo todo
 
 
@@ -14,6 +15,7 @@ btnAdd.addEventListener('click',
             //  prendere valore utente, inserire nell array
             list.push(newSurname.value);
             list.sort();
+
             // costruire elementi lista
             var items = '';
             for (var i = 0; i < list.length; i++) {
